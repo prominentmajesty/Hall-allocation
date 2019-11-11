f29669f3aac6d6cf4c82b1c68fecb239321b157f
MAJESTY <ugochukwuaugustine7@gmail.com>
2019-11-11 11:15:12 -0800

    another commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/hallAllocation',
+    database: 'mongodb+srv://majesty:08136719587@pet-adoption-fvg46.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'barman secret'
 };
